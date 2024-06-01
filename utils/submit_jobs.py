554d06921c301bbb23092b7252a69ec233bdf304
DusanZigic <zigic.dusan1991@gmail.com>
2024-06-01 22:42:16 +0200

    updated submit_jobs in a class

--- utils/submit_jobs.py
+++ utils/submit_jobs.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
 
 from os import path
-from glob import glob
 from subprocess import call, Popen, DEVNULL
-from params import params
 
-def submit_trento_jobs():
-	for job_id in range(params['trento']['num_of_jobs']):
-		job_dir = path.abspath('work/trentojob%d' % job_id)
-		if params['main']['batch_system'] == 'slurm':
-			call('sbatch jobscript.slurm', shell=True, cwd=job_dir, stdout=DEVNULL)
-		else:
-			Popen('bash jobscript.slurm', shell=True, cwd=job_dir, stdout=DEVNULL)
+class submitJobs:
+	def __init__(self, params):
+		self.params = params
 
-def submit_jobs():
-	for job_id in range(params['main']['num_of_jobs']):
-		job_dir = path.abspath('work/job{0:d}'.format(job_id))
-		if params['main']['batch_system'] == 'slurm':
-			call('sbatch jobscript.slurm', shell=True, cwd=job_dir, stdout=DEVNULL)
-		else:
-			Popen('bash jobscript.slurm', shell=True, cwd=job_dir, stdout=DEVNULL)
+	def submit_trento_jobs(self):
+		for job_id in range(self.params['trento']['num_of_jobs']):
+			job_dir = path.abspath(f"work/trentojob{job_id:d}")
+			if self.params['main']['batch_system'] == "slurm":
+				call("sbatch jobscript.slurm", shell=True, cwd=job_dir, stdout=DEVNULL)
+			elif self.params['main']['batch_system'] == "local":
+				Popen("bash jobscript.slurm", shell=True, cwd=job_dir, stdout=DEVNULL)
 
-def submit_eloss_jobs():
-	if params['main']['simulation'] != 'hybrid': return
-	job_dir = path.abspath('work/elossjob')
-	if params['main']['batch_system'] == 'slurm':
-		call('sbatch jobscript.slurm', shell=True, cwd=job_dir, stdout=DEVNULL)
-	else:
-		Popen('bash jobscript.slurm', shell=True, cwd=job_dir, stdout=DEVNULL)
+	def submit_jobs(self):
+		for job_id in range(self.params['main']['num_of_jobs']):
+			job_dir = path.abspath(f"work/job{job_id:d}")
+			if self.params['main']['batch_system'] == "slurm":
+				call("sbatch jobscript.slurm", shell=True, cwd=job_dir, stdout=DEVNULL)
+			elif self.params['main']['batch_system'] == "local":
+				Popen("bash jobscript.slurm", shell=True, cwd=job_dir, stdout=DEVNULL)
+
+	def submit_eloss_jobs(self):
+		if self.params['main']['simulation'] != "hybrid":
+			return
+		job_dir = path.abspath("work/elossjob")
+		if self.params['main']['batch_system'] == "slurm":
+			call("sbatch jobscript.slurm", shell=True, cwd=job_dir, stdout=DEVNULL)
+		elif self.params['main']['batch_system'] == "local":
+			Popen("bash jobscript.slurm", shell=True, cwd=job_dir, stdout=DEVNULL)
