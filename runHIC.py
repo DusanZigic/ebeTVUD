1ea4827103f06fc6f6e3343e3e34692458fa3f49
DusanZigic <zigic.dusan1991@gmail.com>
2024-06-01 23:32:08 +0200

    updated runHIC.py

--- runHIC.py
+++ runHIC.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python3
 
-import sys
-from os import path
 from params import params
-
-from subprocess import call
+from utils.prerequisites import prerequisites
+from utils.update_parameters import update_params
+from utils.generate_jobs import generateJobs
+from utils.submit_jobs import submitJobs
+from utils.collect_data import collectData
 
 if __name__ == '__main__':
 
-	sys.path.insert(1, path.abspath('utils'))
-	import check_prerequisites as cp
-	from update_parameters import update_params
-	import generate_jobs as gj
-	import submit_jobs as sj
-	import collect_data as cd
+	# updating parameters:
+	update_params()
 
-	#####################################################################
-	#updating parameters:
-	if not update_params(): sys.exit()
+	cp = prerequisites(params)
+	gj = generateJobs(params)
+	sj = submitJobs(params)
+	cd = collectData(params)
+	
+	# checking prerequisites and executiables and recompile:
+	cp.check_prerequisites()
+	cp.check_execs()
+	cp.recompile()
 
-	#####################################################################
-	#checking prerequisites and executiables and recompile:
-	if not cp.check_prerequisites(): sys.exit()
-	if not cp.check_execs():		 sys.exit()
-	if not cp.recompile():			 sys.exit()
 
-	#####################################################################
-	#running trento:
+	# trento:
 	gj.gen_trento_jobs()
 	sj.submit_trento_jobs()
 	cd.collect_trento_data()
 
-	#####################################################################
+	# freestream+hydro+frzout+urqmd:
 	gj.gen_jobs()
 	sj.submit_jobs()
 	cd.collect_data()
 
-	#####################################################################
+	# dreena:
 	gj.gen_eloss_jobs()
 	sj.submit_eloss_jobs()
 	cd.collect_eloss_data()
 
-	#####################################################################
-	#collect all model predictions:
+	# collect all model predictions:
 	cd.collect_all()
