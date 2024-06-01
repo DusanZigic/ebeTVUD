a3e893d8dde8f1f74cfafcbd51d7373d1be29d12
DusanZigic <zigic.dusan1991@gmail.com>
2024-06-01 23:36:46 +0200

    updated gen_bcp.py

--- utils/gen_bcp.py
+++ utils/gen_bcp.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-from sys import argv, exit
-from os import path, mkdir, remove, rename
+from os import path
 from re import findall
 import numpy as np
 
 if __name__ == '__main__':
 
-	main_dir      = path.abspath('')
+	main_dir      = path.abspath("")
 	job_id		  = int(findall("\d+", path.split(main_dir)[-1])[0])	
 	trento_events = np.empty((0, 6), float)
 	bcp 	      = np.empty((0, 2), float)
 	
-	with open(path.join(main_dir, 'trento_events.dat'), 'r') as f:
+	with open(path.join(main_dir, "trento_events.dat"), 'r') as f:
 		for file_line in f:
 			line = file_line.rstrip()
 			if line[0] == '#':
@@ -21,7 +20,7 @@ if __name__ == '__main__':
 				data.insert(0, job_id)
 				event_id = int(data[1])
 				trento_events = np.append(trento_events, np.array([data]), axis=0)
-				np.savetxt(path.join(main_dir, 'eventstemp', 'bcp{0:d}.dat'.format(event_id)), bcp, fmt='%5f')
+				np.savetxt(path.join(main_dir, "eventstemp", f"bcp{event_id:d}.dat"), bcp, fmt="%.5f %.5f")
 				bcp = np.empty((0, 2), float)
 			else:
 				data = [float(x) for x in line.split()]
@@ -31,4 +30,5 @@ if __name__ == '__main__':
 	idx = np.empty_like(permutation)
 	idx[permutation] = np.arange(len(permutation))
 	trento_events[:] = trento_events[:, idx]
-	np.savetxt(path.join(main_dir, 'trento_events.dat'), trento_events, fmt='%4d %6d %3d %3d %.5f %.5f', header='job_id event_id npart ncoll TATB b')
+	format, header = "%4d %6d %3d %3d %.5f %.5f", "job_id event_id npart ncoll TATB b"
+	np.savetxt(path.join(main_dir, "trento_events.dat"), trento_events, fmt=format, header=header)
