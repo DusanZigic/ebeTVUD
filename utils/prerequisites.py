4b8af3dd257d33779d8cc23a2b1558a329889af6
DusanZigic <zigic.dusan1991@gmail.com>
2024-06-14 11:51:38 +0200

    added closing compile file

--- utils/prerequisites.py
+++ utils/prerequisites.py
@@ -259,17 +259,21 @@ class prerequisites:
 			for pName in ["Down", "DownBar", "Gluon", "Strange", "Up", "UpBar"]:
 				if not f"ptDist_{self.params['trento']['ecm']:d}GeV_{pName}.dat" in listdir(src_dir):
 					print(f"Error: unable to find initial pT distribution for {pName.lower().replace('bar', '-bar')} quark. Aborting...")
+					compile_file.close()
 					exit()
 			if not path.exists(path.join(model_dir, "DSSFFs")):
 				print("Error: unable to find DSSFFs source directory. Aborting...")
+				compile_file.close()
 				exit()
 		if "d" in self.params['dreena']['particles']:
 			if not f"ptDist_{self.params['trento']['ecm']:d}GeV_Charm.dat" in listdir(src_dir):
 				print(f"Error: unable to find initial pT distribution for charm quark. Aborting...")
+				compile_file.close()
 				exit()
 		if "b" in self.params['dreena']['particles']:
 			if not f"ptDist_{self.params['trento']['ecm']:d}GeV_Bottom.dat" in listdir(src_dir):
 				print(f"Error: unable to find initial pT distribution for bottom quark. Aborting...")
+				compile_file.close()
 				exit()
 
 		
